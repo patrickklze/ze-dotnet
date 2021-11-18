f93e223724f8f5369d030518914927019a31e847
Patrick Lee <peottack@hotmail.com>
2021-11-18 15:06:42 +0800

    upd4

--- AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
@@ -30,7 +30,6 @@ namespace AspNetCoreWebServiceTest.Controllers
             HelloController controller = new HelloController();
             var response = controller.Post().Value as Response;
             Assert.Equal("Hello World!", response.Output);
-            //test
         }
 
         [Theory]
